0da27f17a2e6264adb5220d8b3cfa86cc5f74c1e
Ricky L. T. Wong <rickywonglt15@outlook.com>
2025-03-20 21:49:50 -0400

    Implemented Azure API

--- lib/ai/models.ts
+++ lib/ai/models.ts
@@ -12,9 +12,9 @@ export const chatModels: Array<ChatModel> = [
     name: 'Chat model',
     description: 'Primary model for all-purpose chat',
   },
-  {
-    id: 'chat-model-reasoning',
-    name: 'Reasoning model',
-    description: 'Uses advanced reasoning',
-  },
+  // {
+  //   id: 'chat-model-reasoning',
+  //   name: 'Reasoning model',
+  //   description: 'Uses advanced reasoning',
+  // },
 ];
--- lib/ai/providers.ts
+++ lib/ai/providers.ts
@@ -3,9 +3,10 @@ import {
   extractReasoningMiddleware,
   wrapLanguageModel,
 } from 'ai';
-import { groq } from '@ai-sdk/groq';
-import { xai } from '@ai-sdk/xai';
+// import { groq } from '@ai-sdk/groq';
+// import { xai } from '@ai-sdk/xai';
 import { fal } from '@ai-sdk/fal';
+import { azure } from '@ai-sdk/azure';
 import { isTestEnvironment } from '../constants';
 import {
   artifactModel,
@@ -13,6 +14,8 @@ import {
   reasoningModel,
   titleModel,
 } from './models.test';
+
+
 
 export const myProvider = isTestEnvironment
   ? customProvider({
@@ -24,14 +27,19 @@ export const myProvider = isTestEnvironment
       },
     })
   : customProvider({
+      // languageModels: {
+      //   'chat-model': xai('grok-2-1212'),
+      //   'chat-model-reasoning': wrapLanguageModel({
+      //     model: groq('deepseek-r1-distill-llama-70b'),
+      //     middleware: extractReasoningMiddleware({ tagName: 'think' }),
+      //   }),
+      //   'title-model': xai('grok-2-1212'),
+      //   'artifact-model': xai('grok-2-1212'),
+      // },
       languageModels: {
-        'chat-model': xai('grok-2-1212'),
-        'chat-model-reasoning': wrapLanguageModel({
-          model: groq('deepseek-r1-distill-llama-70b'),
-          middleware: extractReasoningMiddleware({ tagName: 'think' }),
-        }),
-        'title-model': xai('grok-2-1212'),
-        'artifact-model': xai('grok-2-1212'),
+        'chat-model': azure('gpt-4o'),
+        'title-model': azure('gpt-4o'),
+        'artifact-model': azure('gpt-4o'),
       },
       imageModels: {
         'small-model': fal.image('fal-ai/fast-sdxl'),
